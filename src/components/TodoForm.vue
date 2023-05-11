a9250909593e47359edac0b973f3f2f82b5d9b9c
Eric Guyader-Berger <ericguyader@yahoo.fr>
2023-05-11 16:48:06 +0200

    Fin lesson 2 - adds mutation to add todo to store

--- src/components/TodoForm.vue
+++ src/components/TodoForm.vue
@@ -1,9 +1,20 @@
 <script setup>
+    import { ref } from 'vue';
+    import { useTodoListStore } from '@/stores/todoList'
 
+    const todo = ref('')
+    const store = useTodoListStore();
+
+    function addItemAndClear(item){
+        store.addTodo(item);
+        todo.value = '';
+    }
 </script>
 
 <template>
-    
+    <form @submit.prevent="addItemAndClear(todo)">
+        <input v-model="todo" type="text"/><button>Add</button>
+    </form>
 </template>
 
 <style scoped>
--- src/stores/todoList.js
+++ src/stores/todoList.js
@@ -1,9 +1,18 @@
 import { defineStore } from "pinia";
 
 export const useTodoListStore = defineStore('TodoList', {
-    state: {
-        state: () => ({
-            todoList: []
-        })
+    
+    state: () => ({
+        todoList: [],
+        id: 0
+    }),
+    actions: {
+        addTodo(item){
+            this.todoList.push({ item, id: this.id++, completed: false });
+        },
+        deleteItem(itemId){
+            this.todoList = this.todoList.filter( item => item.id !== itemId )
+        }
     }
+
 })
